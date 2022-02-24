0fa2b3e66d0ec53fee4d8d14a23853605092657b
myself-mahadi <myself.hasanmahadi@gmail.com>
2022-02-25 03:47:26 +0600

    feb 25/22

---
+++ 1.two-sum.ts
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1 lang=typescript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+    for (let i: number = 0; i < nums.length; i++){
+        if (nums[i] < target) {
+            for (let j: number = 0; j < nums.length; j++){
+                
+            }
+        }
+    }
+
+    return [];
+};
+// @lc code=end
